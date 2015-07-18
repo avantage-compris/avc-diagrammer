5fe36e1005af93b50b18d0212ae62f1c13fac5a0
David <david.andriana@avantage-compris.com>
2015-07-18 21:09:11 +0200

    Add some comments

--- doc-images.sh
+++ doc-images.sh
@@ -3,10 +3,18 @@
 # Generate SVG and PNG images for the documentation.
 #
 
+######## 1. GENERATE SVG IMAGES FROM JAVA SAMPLES
+
 mvn clean test
+
+######## 2. COPY SVG IMAGES TO src/site/
 
 cp target/*.svg src/site/resources/images/
 
+######## 3. RASTERIZE SVG IMAGES INTO PNG
+
 mvn pre-site
+
+######## 4. COPY PNG IMAGES TO src/site/
 
 cp target/site/images/*.png src/site/resources/rasterized/
