bced303b98ef70caa9fde152d012222fafc03197
David <david.andriana@avantage-compris.com>
2015-07-19 16:50:45 +0200

    Fix: Add a gray arrow

--- src/test/java/net/avcompris/tools/diagrammer/sample/MigrationScenario3_4.java
+++ src/test/java/net/avcompris/tools/diagrammer/sample/MigrationScenario3_4.java
@@ -14,7 +14,7 @@ public class MigrationScenario3_4 extends MigrationScenario3_0 {
 		v0("state=1205506");
 
 		arrow(webapp0, app_data0);
-		arrow(b_converter0, app_data0);
+		arrow_gray(b_converter0, app_data0);
 
 		webapp1();
 		app_data1("state=1205489");
